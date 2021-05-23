894955a11f20046f0d15c5dd2ad23cfba522c945
Jawad3917 <xawadamir0@gamil.com>
2021-05-23 21:51:58 +0600

    end of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -2,11 +2,11 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { IPagination } from '../shared/models/pagination';
-
+import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
-import { IBrand } from '../shared/Models/brands';
-import { IType } from '../shared/models/productType';
+import { IProduct } from '../shared/models/product';
+import {IBrand} from "../shared/Models/brands";
 
 @Injectable({
   providedIn: 'root'
@@ -41,6 +41,10 @@ export class ShopService {
           return response.body;
         })
       )
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
