fe35574d52e5083f946e45e37f2ff60755f74e18
Jawad3917 <xawadamir0@gamil.com>
2021-07-02 01:58:00 +0600

    end of section 17

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,4 +1,3 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -8,19 +7,21 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
-import { BreadcrumbModule } from 'xng-breadcrumb';
+import {BreadcrumbModule} from 'xng-breadcrumb';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
-    BreadcrumbModule,
     RouterModule,
+    BreadcrumbModule,
+    SharedModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
     })
   ],
-  exports: [NavBarComponent,SectionHeaderComponent]
+  exports: [NavBarComponent, SectionHeaderComponent]
 })
 export class CoreModule { }
